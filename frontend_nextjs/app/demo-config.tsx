ebdca0aeb18b3a2c3df779e846094983ddc7761f
Nils Hartmann <nils@nilshartmann.net>
2024-11-06 12:01:54 +0100

    initiale Demo-Konfiguration

--- frontend_nextjs/app/demo-config.tsx
+++ frontend_nextjs/app/demo-config.tsx
@@ -1,22 +1,20 @@
 // initial Demo Konfiguration::::::::::
 
+// Für 2. Demo auf true setzen:
 export const enableSuspenseForFeedback = false;
+
 export const slowDown_IncreaseLikes = 1200;
 export const slowDown_GetFeedbacks = 600;
 export const slowDown_GetRecipe = 0;
 export const showNextLogoInHeader = false;
-// //////////////////////////////////////////////////////
 
-export const enableTanstackQueryDevTools = false;
+// Für Request-Demo auf 0 setzen:
+export const debounce_search = 200;
 
-/**
- * Set to -1 to make searching for longer terms FASTER
- * than searching for shorter terms
- * to show discarding of older requests
- */
 export const slowDown_search = 1200;
 
-export const debounce_search = 0;
+// //////////////////////////////////////////////////////
+export const enableTanstackQueryDevTools = false;
 
 /** number of recipes on /recipes */
 export const recipesPerPage = 6;
