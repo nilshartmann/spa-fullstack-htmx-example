f378ca3c28551f30dc024f0f1db74cf2c3c7463c
Nils Hartmann <nils@nilshartmann.net>
2025-03-17 12:31:15 +0100

    ...

--- frontend_nextjs/app/recipes/search/page.tsx
+++ frontend_nextjs/app/recipes/search/page.tsx
@@ -35,6 +35,8 @@ export default function SearchPage() {
   return (
     <>
       <main className={"flex-grow"}>
+        {/*Funktioniert nicht - womöglich buggy in der RC Version von Next*/}
+        <title>{search} - Recipify Next.js Demo</title>
         <div className="container mx-auto mt-8 flex max-w-96 pt-8">
           <Input
             type="search"
